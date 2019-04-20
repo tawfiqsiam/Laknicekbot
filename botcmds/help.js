43286c0f536b17df6f6480fc528fc8bb1ed48e94
Laknicek <39445505+Laknicek@users.noreply.github.com>
2019-04-21 00:52:21 +0300

    Update help.js

--- botcmds/help.js
+++ botcmds/help.js
@@ -9,7 +9,7 @@ module.exports.run = async (bot, message, args) => {
   let helpembed = new Discord.RichEmbed()
   .setTitle("~Command Handler By Laknicek!~")
   .setColor("RANDOM")
-  .setDescription(`Prefix is \`\`c!\`\`\nUsage : **c!help-<module>**\nTo get help with a specified module!\n\n**Available Modules** : currency, fun , staff , helpful , bot\n\nExample : **c!help-fun**`)
+  .setDescription(`Prefix is \`\`c!\`\`\nUsage : **c!help-<module>**\nTo get help with a specified module!\n\n**Available Modules** :🙌🏼 currency, fun , staff , helpful , bot\n\nExample 🙌🏼: **c!help-fun**`)
   .setFooter(`Requested by ${message.author.tag}`)
   
   message.channel.send(helpembed)
