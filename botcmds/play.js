8a7cefc6adb3d0a5a57febd19cc6808693765291
Laknicek <39445505+Laknicek@users.noreply.github.com>
2019-04-22 17:15:26 +0300

    Update play.js

--- botcmds/play.js
+++ botcmds/play.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
    const prefix = "c!";
   if(!message.content.startsWith(prefix))return;
   
- //if(message.author.id !== "350956268015452161" || "352815845707087872" || "402096246199943170") return message.channel.send("This feature is now premium only! Use \*\*c!premium\*\* to upgrade to premium!");
+ //if(message.author.id !== "277829088461193216" || "352815845707087872" || "402096246199943170") return message.channel.send("This feature is now premium only! Use \*\*c!premium\*\* to upgrade to premium!");
   
 if (!message.member.voiceChannel) return message.channel.send("Please join a voice channel first!");
   
