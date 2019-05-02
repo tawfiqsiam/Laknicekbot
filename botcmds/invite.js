ca39771b91fcdedf83356442f82b4ac6a80f9387
Laknicek <39445505+Laknicek@users.noreply.github.com>
2019-05-02 14:52:53 +0300

    Update invite.js

--- botcmds/invite.js
+++ botcmds/invite.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
   let target = message.mentions.users.first() || message.author;
   let inviteEmbed = new Discord.RichEmbed()
   .setTitle("Invitation!")
-  .setDescription("\n[Click me](https://discordapp.com/api/oauth2/authorize?client_id=551044385735835648&permissions=0&scope=bot) to invite the bot!\n\nOr:\n\n!\n\nRequested by:")
+  .setDescription("\n[Click me To invite The Bot Without Perms](https://discordapp.com/api/oauth2/authorize?client_id=551044385735835648&permissions=0&scope=bot) to invite the bot!\n\nOr:\n\n[With perms] https://discordapp.com/api/oauth2/authorize?client_id=551044385735835648&permissions=8&scope=bot\n\nRequested by:")
   .setColor("RANDOM")
   .setFooter(target.username, target.avatarURL)
  
