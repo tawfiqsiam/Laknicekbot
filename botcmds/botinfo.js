71fc23ad9c62c5f4e792b936ac52d2449ae3818d
Laknicek <39445505+Laknicek@users.noreply.github.com>
2019-05-02 15:42:39 +0300

    Update botinfo.js

--- botcmds/botinfo.js
+++ botcmds/botinfo.js
@@ -7,16 +7,16 @@ module.exports.run = async (bot, message, args) => {
     
 let bicon = bot.user.displayAvatarURL
     let botembed = new Discord.RichEmbed()
-    .setDescription("Bot information")
+    .setDescription("***Bot information***")
     .setColor("#15f153")
     .setThumbnail(bicon)
-    .addField("Developer", "⎝⧹๖̶̶̶ζ[Laknicek]⧸⎠#0279", true)
-    .addField("What I Have", "Fun Commands | Staff Commands | Currency Commands | Helpful Commands | Bot Commands", true)
-    .addField("Servers", `${bot.guilds.size} servers!`, true)
-    .addField("Users", `${bot.users.size} users!`, true)
-    .addField("Channels", `${bot.channels.size} channels!`, true) 
-    .addField("Bot Name", bot.user.tag, true)
-    .addField("Official Discord Server", "https://discord.gg/Qy33V6h", true)
+    .addField("***Developer***", "⎝⧹๖̶̶̶ζ[Laknicek]⧸⎠#0279", true)
+    .addField("***What I Have***", "Fun Commands | Staff Commands | Currency Commands | Helpful Commands | Bot Commands", true)
+    .addField("***Servers***", `${bot.guilds.size} servers!`, true)
+    .addField("***Users***", `${bot.users.size} users!`, true)
+    .addField("***Channels***", `${bot.channels.size} channels!`, true) 
+    .addField("***Bot Name***", bot.user.tag, true)
+    .addField("***Official Discord Server***", "https://discord.gg/Qy33V6h", true)
     message.channel.send(botembed);
     }
 
