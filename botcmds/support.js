ddbbca32dc52db6f5a1422b5ed735660c67fa61f
Laknicek <39445505+Laknicek@users.noreply.github.com>
2019-05-11 11:19:18 +0300

    Update support.js

--- botcmds/support.js
+++ botcmds/support.js
@@ -9,7 +9,7 @@ module.exports.run = async (bot, message, args) => {
   .setTitle("**Support/Tips**")
   .setDescription("**Those are tips if the bot is not working**")
   .setColor("RANDOM")
-  .addField("If there are no logs of bans/kicks form CipBot", "**Make a channel called logs**", true)
+  .addField("If there are no logs of bans/kicks form Laknicek-Bot", "**Make a channel called logs**", true)
   .addField("If the ban / kick commands are not working", "**Be sure that the bot has Administrator Permissions**", true)
   .setAuthor(message.author.tag)
 
